0082e2df58dcf2e890e8bbacba98912d5f9ba62d
Junseo Bae <ferrater1013@gmail.com>
2025-08-03 18:02:39 +0900

    refactor: Controller - 세션 관리 역할 구분

--- src/main/java/darak/community/core/session/SessionManager.java
+++ src/main/java/darak/community/core/session/SessionManager.java
@@ -2,19 +2,32 @@ package darak.community.core.session;
 
 import darak.community.core.session.constant.SessionConst;
 import darak.community.core.session.dto.LoginMember;
+import darak.community.domain.member.Member;
+import darak.community.infra.adaptor.MemberRepositoryAdaptor;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class SessionManager {
 
-    public void login(HttpSession session, LoginMember loginMember) {
-        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
-    }
+    private final MemberRepositoryAdaptor memberRepositoryAdaptor;
 
     public void logout(HttpSession session) {
         if (session != null) {
             session.invalidate();
         }
+    }
+
+    public void login(HttpSession session, @NotEmpty String loginId) {
+        Member member = memberRepositoryAdaptor.findByLoginId(loginId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+        registerMemberIn(session, LoginMember.of(member.getId(), member.getMemberGrade()));
+    }
+
+    private void registerMemberIn(HttpSession session, LoginMember loginMember) {
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
     }
 }
